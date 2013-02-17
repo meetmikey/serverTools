e734bb8e592235eef63fdc4d6a55102ab745a1b0
Justin Durack <justin@magicnotebook.com>
2013-02-16 19:42:36 -0800

    script updates

--- bootstrap/buildDevServer.sh
+++ bootstrap/buildDevServer.sh
@@ -2,9 +2,26 @@
 
 mkdir -p $MIKEY_SOURCE
 
-git clone git@github.com:meetmikey/serverCommon.git $MIKEY_SOURCE/serverCommon
-git clone git@github.com:meetmikey/mikeymail.git $MIKEY_SOURCE/mikeymail
-git clone git@github.com:meetmikey/mailReader.git $MIKEY_SOURCE/mailReader
-git clone git@github.com:meetmikey/mikeyAPI.git $MIKEY_SOURCE/mikeyAPI
+BRANCH="master"
+
+REPOSITORIES=( serverCommon mikeymail mailReader mikeyAPI )
+
+#Clone only needs to run the first time.
+#git clone git@github.com:meetmikey/serverCommon.git $MIKEY_SOURCE/serverCommon
+#git clone git@github.com:meetmikey/mikeymail.git $MIKEY_SOURCE/mikeymail
+#git clone git@github.com:meetmikey/mailReader.git $MIKEY_SOURCE/mailReader
+#git clone git@github.com:meetmikey/mikeyAPI.git $MIKEY_SOURCE/mikeyAPI
+
+for i in "${REPOSITORIES[@]}"
+do
+  cd $MIKEY_SOURCE/$i
+  git pull origin $BRANCH  
+done
 
 cp -r $MIKEY_SOURCE/* $MIKEY_BUILD/
+
+for i in "${REPOSITORIES[@]}"
+do
+  cd $MIKEY_BUILD/$i
+  npm install
+done
--- bootstrap/setupServer.sh
+++ bootstrap/setupServer.sh
@@ -2,15 +2,20 @@
 
 yum update -y
 
+yum install -y gcc-c++ make openssl-devel
+
+
 mkdir -p /usr/local/source
 cd /usr/local/source
 
-wget http://nodejs.org/dist/v0.8.15/node-v0.8.15.tar.gz
-tar zxvf node-v0.8.15.tar.gz
-cd node-v0.8.15
+wget http://nodejs.org/dist/v0.8.20/node-v0.8.20.tar.gz
+tar xvzf node-v0.8.20.tar.gz
+cd node-v0.8.20-linux-x64
 ./configure
 make
 make install
 
-mkdir -p /usr/local/mikey
-chown -R mikey:mikey /usr/local/mikey
+MIKEY_BUILD="/usr/local/mikey"
+
+mkdir -p $MIKEY_BUILD
+chown -R mikey:mikey $MIKEY_BUILD
