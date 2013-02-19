37a5077d75ddb17dfb3054fdee0a3cc75a6f8ca3
Justin Durack <justin@magicnotebook.com>
2013-02-18 22:05:41 -0800

    closer to build

--- bootstrap/buildDevServer.sh
+++ bootstrap/buildDevServer.sh
@@ -17,15 +17,18 @@ for i in "${REPOSITORIES[@]}"
 do
   cd $MIKEY_SOURCE/$i
   git pull origin $BRANCH
+  npm install
   rm -rf $MIKEY_BUILD/$i
   rsync -r --exclude=.git $MIKEY_SOURCE/* $MIKEY_BUILD/
   cd $MIKEY_BUILD/$i
-  npm install
 done
+
+/usr/local/elasticsearch/bin/service/elasticsearch stop
+/usr/local/elasticsearch/bin/service/elasticsearch start
 
 for i in "${PROGRAMS[@]}"
 do
   cd $MIKEY_BUILD/$i
-  stop.sh
-  start.sh
+  ./stop.sh
+  ./start.sh
 done
--- bootstrap/setupServer.sh
+++ bootstrap/setupServer.sh
@@ -4,11 +4,10 @@ yum update -y
 
 yum install -y gcc-c++ make openssl-devel git
 
-
 mkdir -p /usr/local/source
 cd /usr/local/source
 
-#NODE
+#node
 wget http://nodejs.org/dist/v0.8.20/node-v0.8.20.tar.gz
 tar xvzf node-v0.8.20.tar.gz
 cd node-v0.8.20-linux-x64
@@ -18,7 +17,10 @@ make install
 cd ../
 rm -f node-v0.8.20.tar.gz
 
-#SCONS (needed to build mongodb)
+#forever
+npm install forever -g
+
+#scons (needed to build mongodb)
 wget http://sourceforge.net/projects/scons/files/scons/2.2.0/scons-2.2.0.tar.gz/download
 tar xvzf scons-2.2.0.tar.gz
 cd scons-2.2.0
@@ -26,7 +28,7 @@ python setup.py install
 cd ../
 rm -f scons-2.2.0.tar.gz
 
-#MONGODB
+#mongodb
 wget http://downloads.mongodb.org/src/mongodb-src-r2.2.3.tar.gz
 tar xvzf mongodb-src-r2.2.3.tar.gz
 cd mongodb-src-r2.2.3
@@ -35,14 +37,16 @@ scons --prefix=/opt/mongo install
 cd ../
 rm -f mongodb-src-r2.2.3.tar.gz
 
-
+#elastic search
 wget http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.5.tar.gz
 tar xvzf elasticsearch-0.20.5.tar.gz
-cd elasticsearch-0.20.5
-#what now?
+cp -r elasticsearch-0.20.5 /usr/local/
+ln -s /usr/local/elasticsearch-0.20.5/ /usr/local/elasticsearch
+cp ../config/elasticsearch.yml /usr/local/elasticsearch/config/
 
-
-wget http://wrapper.tanukisoftware.com/download/3.5.17/wrapper-linux-x86-32-3.5.17-pro.tar.gz
+#elastic search service wrapper
+git clone https://github.com/elasticsearch/elasticsearch-servicewrapper.git
+cp -r elasticsearch-servicewrapper/service /usr/local/elasticsearch/bin/
 
 
 MIKEY_BUILD="/usr/local/mikey"
