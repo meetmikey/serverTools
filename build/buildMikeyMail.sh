c118d282e3a461deb32bec40ba685dffcb35fedd
Sagar Mehta <mehta.sagarv@gmail.com>
2013-03-14 22:01:23 -0700

    way to only build mikeyMail

---
+++ build/buildMikeyMail.sh
@@ -0,0 +1,28 @@
+mkdir -p $MIKEY_SOURCE
+
+BRANCH="master"
+
+REPOSITORIES=( serverTools serverCommon mikeymail )
+PROGRAMS=( mikeymail )
+
+#Clone only needs to run the first time.
+#git clone https://meetmikeygit:delos%5pass@github.com/meetmikey/serverCommon.git $MIKEY_SOURCE/serverCommon
+#git clone https://meetmikeygit:delos%5pass@github.com/meetmikey/mikeymail.git $MIKEY_SOURCE/mikeymail
+#git clone https://meetmikeygit:delos%5pass@github.com/meetmikey/mailReader.git $MIKEY_SOURCE/mailReader
+
+for i in "${REPOSITORIES[@]}"
+do
+  cd $MIKEY_SOURCE/$i
+  git pull origin $BRANCH
+  npm install
+  rm -rf $MIKEY_BUILD/$i
+  rsync -rq --exclude=.git $MIKEY_SOURCE/* $MIKEY_BUILD/
+  cd $MIKEY_BUILD/$i
+done
+
+for i in "${PROGRAMS[@]}"
+do
+  cd $MIKEY_BUILD/$i
+  ./stop.sh
+  ./start.sh
+done
