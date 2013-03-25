bae2dd15f64c3b9de4ae5545980e975c515670e4
Sagar Mehta <mehta.sagarv@gmail.com>
2013-03-24 19:20:57 -0700

    es migration build

---
+++ build/buildESMigrator.sh
@@ -0,0 +1,20 @@
+mkdir -p $MIKEY_SOURCE
+
+BRANCH="master"
+
+REPOSITORIES=( serverTools serverCommon esMigration )
+
+#Clone only needs to run the first time.
+#git clone https://meetmikeygit:delos%5pass@github.com/meetmikey/serverCommon.git $MIKEY_SOURCE/serverCommon
+#git clone https://meetmikeygit:delos%5pass@github.com/meetmikey/mikeyAPI.git $MIKEY_SOURCE/mikeyAPI
+git clone https://meetmikeygit:delos%5pass@github.com/meetmikey/mikeyAPI.git $MIKEY_SOURCE/esMigration
+
+for i in "${REPOSITORIES[@]}"
+do
+  cd $MIKEY_SOURCE/$i
+  git pull origin $BRANCH
+  npm install
+  rm -rf $MIKEY_BUILD/$i
+  rsync -rq --exclude=.git $MIKEY_SOURCE/* $MIKEY_BUILD/
+  cd $MIKEY_BUILD/$i
+done
