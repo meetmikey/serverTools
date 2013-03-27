ce7d5d102dd9ad92562c5198977ebdb88227c8bc
Sagar Mehta <mehta.sagarv@gmail.com>
2013-03-27 16:19:14 -0700

    more logging around sockets

--- scripts/requeueMessages.js
+++ scripts/requeueMessages.js
@@ -5,7 +5,7 @@ var winston = require (serverCommon + '/lib/winstonWrapper').winston,
     cloudStorageUtils = require (serverCommon + '/lib/cloudStorageUtils'),
     appInitUtils = require (serverCommon + '/lib/appInitUtils'),
     conf = require (serverCommon + '/conf'),
-    http = require ('http'),
+    https = require ('https'),
     mongoose = require (serverCommon + '/lib/mongooseConnect').mongoose;
 
 var MailModel = mongoose.model ('Mail');
@@ -14,7 +14,6 @@ var initActions = [
   appInitUtils.CONNECT_MONGO
 ];
 
-console.log (http.globalAgent);
 
 appInitUtils.initApp( 'requeueMessages', initActions, conf, function() {
 
@@ -26,11 +25,19 @@ appInitUtils.initApp( 'requeueMessages', initActions, conf, function() {
       winston.doMongoError ('Could not get mail', {err : err});
     }
     else if (foundMails) {
+
+
       winston.doInfo ('About to send messages to queue', {numMessages : foundMails.length});
 
       foundMails.forEach (function (mail) {
         var inAzure = true;
         sqsConnect.addMessageToMailReaderQueue ({'userId' : mail.userId, 'path' : mail.s3Path, 'mailId' : mail._id, 'inAzure' : inAzure});
+
+        console.log (https.globalAgent.sockets['sqs.us-east-1.amazonaws.com:443'].length)
+        console.log ('sockets', Object.keys(https.globalAgent.sockets).length);
+        console.log ('requests', Object.keys(https.globalAgent.requests).length);
+
+
       });
     }
   });
