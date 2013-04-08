4bbc467bb8b11b087bcd2d307c57c1d21273377e
Justin Durack <justin@magicnotebook.com>
2013-04-08 16:26:22 -0700

    added nrpe.cfg copy

--- bootstrap/nrpe/deployNRPE.sh
+++ bootstrap/nrpe/deployNRPE.sh
@@ -2,6 +2,10 @@
 
 NAGIOS_PATH=/usr/local/nagios
 
+cp etc/nrpe.cfg $NAGIOS_PATH/etc/
+chown nagios:nagios $NAGIOS_PATH/etc/nagios.cfg
+chmod 664 $NAGIOS_PATH/etc/nagios.cfg
+
 mkdir -p $NAGIOS_PATH/libexec/custom
 chown nagios:nagios $NAGIOS_PATH/libexec/custom
 cp libexec/custom/* $NAGIOS_PATH/libexec/custom/
