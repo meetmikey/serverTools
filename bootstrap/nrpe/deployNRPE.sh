c1ccb0af9a842581727783856a9cc06e00e91d2c
Justin Durack <justin@magicnotebook.com>
2013-04-08 16:27:03 -0700

    typo

--- bootstrap/nrpe/deployNRPE.sh
+++ bootstrap/nrpe/deployNRPE.sh
@@ -3,8 +3,8 @@
 NAGIOS_PATH=/usr/local/nagios
 
 cp etc/nrpe.cfg $NAGIOS_PATH/etc/
-chown nagios:nagios $NAGIOS_PATH/etc/nagios.cfg
-chmod 664 $NAGIOS_PATH/etc/nagios.cfg
+chown nagios:nagios $NAGIOS_PATH/etc/nrpe.cfg
+chmod 664 $NAGIOS_PATH/etc/nrpe.cfg
 
 mkdir -p $NAGIOS_PATH/libexec/custom
 chown nagios:nagios $NAGIOS_PATH/libexec/custom
