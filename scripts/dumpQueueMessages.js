407c0623950873f63c83f3f87e1eaf0afb357d02
Sagar Mehta <mehta.sagarv@gmail.com>
2013-04-11 19:50:42 -0700

    dump queue messages script is better

--- scripts/dumpQueueMessages.js
+++ scripts/dumpQueueMessages.js
@@ -6,6 +6,31 @@ var winston = require (serverCommon + '/lib/winstonWrapper').winston,
     conf = require (serverCommon + '/conf');
 
 
-sqsConnect.pollMailReaderQueue (function (message, pollQueueCallback) {
+var queueName = process.argv[2];
+var numWorkers = 20;
+
+if (!queueName) {
+  winston.doError ("must specify queue which you wish to clear");
+  process.exit(1);
+}
+
+queueName = queueName.toLowerCase();
+
+var pollFunction;
+
+if (queueName == 'worker') {
+  pollFunction = sqsConnect.pollWorkerQueue;
+} else if (queueName == 'mailreader') {
+  pollFunction = sqsConnect.pollMailReaderQueue;
+} else if (queueName == 'mailreaderquick') {
+  pollFunction = sqsConnect.pollMailReaderQuickQueue;
+} else if (queueName == 'maildownload') {
+  pollFunction = sqsConnect.pollMailDownloadQueue;
+} else {
+  winston.doError ('invalid queue name');
+  process.exit(1);
+}
+
+pollFunction (function (message, pollQueueCallback) {
   pollQueueCallback ()
-}, 80);
+}, numWorkers);
