bc999c1d96850d278c875dd0f0d859ffa40f855c
Sagar Mehta <mehta.sagarv@gmail.com>
2013-04-16 19:11:38 -0700

    script to delete drafts

---
+++ scripts/expungeDrafts.js
@@ -0,0 +1,59 @@
+var serverCommon = process.env.SERVER_COMMON;
+
+var winston = require (serverCommon + '/lib/winstonWrapper').winston,
+    sqsConnect = require (serverCommon + '/lib/sqsConnect'),
+    cloudStorageUtils = require (serverCommon + '/lib/cloudStorageUtils'),
+    appInitUtils = require (serverCommon + '/lib/appInitUtils'),
+    conf = require (serverCommon + '/conf'),
+    https = require ('https'),
+    mongoose = require (serverCommon + '/lib/mongooseConnect').mongoose;
+
+var MailModel = mongoose.model ('Mail');
+var LinkModel = mongoose.model ('Link');
+var AttachmentModel = mongoose.model ('Attachment')
+
+var initActions = [
+  appInitUtils.CONNECT_MONGO
+];
+
+var limit = 100;
+
+if (process.argv.length > 2) {
+  limit = parseInt (process.argv[2]);
+  console.log ('limit', limit);
+}
+
+appInitUtils.initApp( 'expungeDrafts', initActions, conf, function() {
+
+  MailModel.find ({mmDone : true, gmLabels : '\\\\Draft'}, 
+    '_id userId uid gmLabels',
+    {limit : limit},
+    function (err, foundMails) {
+    if (err) {
+      winston.doMongoError ('Could not get mail', {err : err});
+    }
+    else if (foundMails) {
+
+      winston.doInfo ('About to delete links attachments corresponding to drafts mails ', {numMessages : foundMails.length});
+
+      foundMails.forEach (function (mail) {
+        console.log (mail);
+        /*
+        // delete links, attachments, that originated from the draft
+        LinkModel.remove (mailId : mail._id, function (err) {
+          if (err) {
+            winston.doMongoError (err);
+          }
+        });
+
+        AttachmentModel.remove (mailId : mail._id, function (err) {
+          if (err) {
+            winston.doMongoError (err);
+          }
+        });
+        */
+      });
+    }
+  });
+
+});
