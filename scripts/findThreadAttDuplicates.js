db76173d8e5d4ab8bfafd8148a6f8d8e5c080b18
Sagar Mehta <mehta.sagarv@gmail.com>
2013-04-16 22:57:25 -0700

    asynced it

--- scripts/findThreadAttDuplicates.js
+++ scripts/findThreadAttDuplicates.js
@@ -84,40 +84,57 @@ appInitUtils.initApp( 'findThreadAttDuplicates', initActions, conf, function() {
     function (err) {
       if (err) {
         winston.handleError (err);
+      } else {
+        console.log ('all done for all users');
       }
     });
   });
 
-  function findDupes (userId, cb) {
+  function findDupes (userId, findDupesCb) {
     // for each promoted attachment
     AttachmentModel.find ({userId : userId})
       .limit (limit)
       .select ('gmThreadId hash')
       .exec (function (err, attachments) {
         if (err) {
-          cb (winston.makeMongoError (err));
+          findDupesCb (winston.makeMongoError (err));
         } 
         else if (attachments && attachments.length) {
-          attachments.forEach ( function (attachment)  {
+
+          async.each (attachments, function (attachment, asyncCb)  {
             var hash = attachment.hash;
             var gmThreadId = attachment.gmThreadId;
 
             AttachmentModel.count ({userId : userId, gmThreadId : gmThreadId, hash : hash}, function (err, count) {
               if (err) {
-                cb (winston.makeMongoError (err));
+                asyncCb (winston.makeMongoError (err));
               }
               else if (count > 1 && !(gmThreadId + "_"  + hash in reported)) {
                 reported [gmThreadId + "_" + hash] = 1;
                 dupes+=1
                 total +=1
-                deleteDupes (userId, gmThreadId, hash, cb);
+                deleteDupes (userId, gmThreadId, hash, asyncCb);
               } 
               else {
                 console.log (count)
                 total +=1
+                asyncCb ();
               }
             });
+
+          },
+          function (err) {
+            if (err) {
+              findDupesCb (err);
+            } else {
+              console.log ('find dupes callback, user complete', userId);
+              findDupesCb ();
+            }
           });
+        }
+        else {
+          console.log ('find dupes callback, user complete', userId);
+          findDupesCb ();
         }
       })
   }
@@ -149,6 +166,7 @@ appInitUtils.initApp( 'findThreadAttDuplicates', initActions, conf, function() {
               cb (winston.makeMongoError (err));
             } else {
               console.log ('delete successful')
+              cb ();
             }
           })
 
