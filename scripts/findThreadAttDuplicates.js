9e1fec03c85c25c1b66831b6cb8ebf628b684333
Sagar Mehta <mehta.sagarv@gmail.com>
2013-04-16 22:29:50 -0700

    wrote code to eventually delete duplicate... though need to test deletion function...

--- scripts/findThreadAttDuplicates.js
+++ scripts/findThreadAttDuplicates.js
@@ -5,6 +5,7 @@ var winston = require (serverCommon + '/lib/winstonWrapper').winston,
     cloudStorageUtils = require (serverCommon + '/lib/cloudStorageUtils'),
     appInitUtils = require (serverCommon + '/lib/appInitUtils'),
     conf = require (serverCommon + '/conf'),
+    async = require ('async'),
     https = require ('https'),
     mongoose = require (serverCommon + '/lib/mongooseConnect').mongoose;
 
@@ -23,7 +24,7 @@ if (process.argv.length > 2) {
   limit = parseInt (process.argv[2]);
   console.log ('limit', limit);
 }
-
+/*
 appInitUtils.initApp( 'findThreadAttDuplicates', initActions, conf, function() {
   var reported = {}
   var dupes = 0;
@@ -66,5 +67,91 @@ appInitUtils.initApp( 'findThreadAttDuplicates', initActions, conf, function() {
 setInterval (function () {
   console.log (dupes/total)
 }, 5000)
+
+});
+*/
+
+appInitUtils.initApp( 'findThreadAttDuplicates', initActions, conf, function() {
+  var reported = {}
+  var dupes = 0;
+  var total = 0;
+  // for each user
+  UserModel.find ({}, function (err, foundUsers) {
+
+    async.eachSeries (foundUsers, function (user, cb) {
+      findDupes (user._id, cb);
+    }, 
+    function (err) {
+      if (err) {
+        winston.handleError (err);
+      }
+    });
+  });
+
+  function findDupes (userId, cb) {
+    // for each promoted attachment
+    AttachmentModel.find ({userId : userId})
+      .limit (limit)
+      .select ('gmThreadId hash')
+      .exec (function (err, attachments) {
+        if (err) {
+          cb (winston.makeMongoError (err));
+        } 
+        else if (attachments && attachments.length) {
+          attachments.forEach ( function (attachment)  {
+            var hash = attachment.hash;
+            var gmThreadId = attachment.gmThreadId;
+
+            AttachmentModel.count ({userId : userId, gmThreadId : gmThreadId, hash : hash}, function (err, count) {
+              if (err) {
+                cb (winston.makeMongoError (err));
+              }
+              else if (count > 1 && !(gmThreadId + "_"  + hash in reported)) {
+                reported [gmThreadId + "_" + hash] = 1;
+                dupes+=1
+                total +=1
+                deleteDupes (userId, gmThreadId, hash, cb);
+              } 
+              else {
+                console.log (count)
+                total +=1
+              }
+            });
+          });
+        }
+      })
+  }
+
+  function deleteDupes (userId, gmThreadId, hash, cb) {
+    var filter = {userId : userId, gmThreadId : gmThreadId, hash : hash};
+    AttachmentModel.find (filter)
+      .select ('gmThreadId hash sentDate')
+      .exec (function (err, dupes) {
+
+        if (err) {
+          cb (winston.makeMongoError (err));
+        } else if (dupes && dupes.length) {
+          var earliestDate = dupes[0].sentDate;
+
+          dupes.forEach (function (dupe) {
+            if (dupe.sentDate < earliestDate) {
+              dupe.earliestDate = dupe.sentDate;
+            }
+          });
+
+          console.log (filter);
+          console.log (earliestDate);
+
+          // TODO : actually delete the duplicate
+          
+
+        }
+      });
+  }
+
+
+  setInterval (function () {
+    console.log (dupes/total)
+  }, 5000)
 
 });
