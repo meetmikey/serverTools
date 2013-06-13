177275246310661fe6bccd7168646a01f7df2a1f
Sagar Mehta <mehta.sagarv@gmail.com>
2013-06-13 09:47:55 -0700

    delete user updated once more

--- scripts/es/search/searchDocs.sh
+++ scripts/es/search/searchDocs.sh
@@ -1,1 +1,1 @@
-curl -XGET 'localhost:9200/mail_v1/document/_search?pretty&fields=file,title,content_type,size,isLink'
+curl -XGET 'localhost:9200/v3/document/_search?pretty&fields=file,title,content_type,size,isLink'
