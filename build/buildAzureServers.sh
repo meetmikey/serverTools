7fc74d9d356112c6bd602e1978c2e00c5a323948
Justin Durack <justin@magicnotebook.com>
2013-07-21 23:39:23 -0700

    fixed typo

--- build/buildAzureServers.sh
+++ build/buildAzureServers.sh
@@ -1,8 +1,7 @@
-#azureConnections=( azureMail1 azureMail2 azureMail3 azureMail4 azureMail5 azureMail6 azureMail7 )
-azureConnections=( azureMail1 )
+azureConnections=( azureMail1 azureMail2 azureMail3 azureMail4 azureMail5 azureMail6 azureMail7 )
 
 for i in "${azureConnections[@]}"
 do
   echo "building $i"
-  ssh $i cd /home/mikey/source/serverTools/build/buildAzureServer.sh
+  ssh $i /home/mikey/source/serverTools/build/buildAzureServer.sh
 done
