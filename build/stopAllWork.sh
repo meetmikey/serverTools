ea5338d5a9f57fb87227b789105071eb30c9a49e
Justin Durack <justin@meetmikey.com>
2013-07-30 13:26:14 -0700

    added "now" feature to stop scripts

--- build/stopAllWork.sh
+++ build/stopAllWork.sh
@@ -1,15 +1,31 @@
+
+#usage: ./stopAllWork <program> <"now"> (both optional)
+#e.g. ./stopAllWork (stops all work, NOT immediately)
+#e.g. ./stopAllWork mailReader (stops mailReader, NOT immediately)
+#e.g. ./stopAllWork mailReader now (stops mailReader, immediately)
+#e.g. ./stopAllWork now (stops all work, immediately)
+
 program=$1
+now=""
+
+if [ "$program" != "" -a "$program" != "now" ];
+	then
+		now=$2
+else
+	program=""
+	now=$1
+fi
 
 machines=( worker1 worker2 azureMail1 azureMail2 azureMail3 azureMail4 azureMail5 azureMail6 azureMail7 )
 
 for i in "${machines[@]}"
 do
-	if [ $program ];
+	if [ "$program" != "" ];
 		then
-			echo "stopping $program on $i"
-		  ssh $i /home/mikey/source/serverTools/build/stopWork.sh $program
+			echo "stopping $program on $i $now"
+		  ssh $i /home/mikey/source/serverTools/build/stopWork.sh $program $now
 		else
-		  echo "stopping all work on $i"
-		  ssh $i /home/mikey/source/serverTools/build/stopWork.sh
+		  echo "stopping all work on $i $now"
+		  ssh $i /home/mikey/source/serverTools/build/stopWork.sh $now
 	fi
 done
--- build/stopWork.sh
+++ build/stopWork.sh
@@ -1,22 +1,38 @@
+
+#usage: ./stopWork <program> <"now"> (both optional)
+#e.g. ./stopWork (stops all work, NOT immediately)
+#e.g. ./stopWork mailReader (stops mailReader, NOT immediately)
+#e.g. ./stopWork mailReader now (stops mailReader, immediately)
+#e.g. ./stopWork now (stops all work, immediately)
+
 program=$1
+now=""
+
+if [ "$program" != "" -a "$program" != "now" ];
+	then
+		now=$2
+else
+	program=""
+	now=$1
+fi
 
 baseDir=/usr/local/mikey
 programs=( mailReader mikeymail workers )
 
-if [ $program ];
+if [ "$program" != "" ];
 	then
 		programDir=$baseDir/$program
 	  if [ -d $programDir ]; then
-	  	echo "stopping $program"
-		  $programDir/stop.sh
+	  	echo "stopping $program $now"
+		  $programDir/stop.sh $now
 		fi
 	else
 		for i in "${programs[@]}"
 		do
 		  programDir=$baseDir/$i
 		  if [ -d $programDir ]; then
-		  	echo "stopping $i"
-			  $programDir/stop.sh
+		  	echo "stopping $i $now"
+			  $programDir/stop.sh $now
 			fi
 		done
 fi
