3ffd54f833ed436b863fdd4e7b3f3a10158264db
Sagar Mehta <mehta.sagarv@gmail.com>
2013-08-13 19:05:30 -0700

    alwasy restart now

--- build/restartAPIServers.sh
+++ build/restartAPIServers.sh
@@ -3,6 +3,6 @@ machines=( api1 api2 )
 for i in "${machines[@]}"
 
 do
-  echo "stopping mikeyAPI on $i $now"
-  ssh $i /home/mikey/source/serverTools/build/startProgram.sh mikeyAPI $now
+  echo "stopping mikeyAPI on $i now"
+  ssh $i /home/mikey/source/serverTools/build/startProgram.sh mikeyAPI now
 done
