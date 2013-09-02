8a7fd041c4ed3fc327dac789fea9e7eca35bf250
Justin Durack <justin@meetmikey.com>
2013-09-02 13:01:41 -0700

    improved restartAPIServers.sh

--- build/restartAPIServers.sh
+++ build/restartAPIServers.sh
@@ -1,8 +1,26 @@
+
 machines=( api1 api2 api3 )
+instance_api1=i-421a0331
+instance_api2=i-401a0333
+instance_api3=i-f6adda90
+
+loadBalancer=mikeyAPILoadBalancer
+waitTimeSeconds=15
 
 for i in "${machines[@]}"
 
 do
-  echo "stopping mikeyAPI on $i now"
+  instanceVar=instance_$i
+  instance=${!instanceVar}
+  echo "pulling $i out of the load balancer..."
+  elb-deregister-instances-from-lb $loadBalancer --instances $instance
+  echo "waiting for traffic to stop on $i..."
+  sleep $waitTimeSeconds
+  echo "restarting mikeyAPI on $i..."
   ssh $i /home/mikey/source/serverTools/build/startProgram.sh mikeyAPI now
+  echo "putting $i back in the load balancer..."
+  elb-register-instances-with-lb $loadBalancer --instances $instance
+  echo "waiting for traffic to start on $i..."
+  sleep $waitTimeSeconds
+  echo "done restarting mikeyAPI on $i."
 done
